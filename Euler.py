cfe7bead60302b12147cb1db1fc06d9a5bfa98f8
André Barata <andre.quina.barata@gmail.com>
2015-09-23 15:44:48 +0000

    Euler comments

--- Euler.py
+++ Euler.py
@@ -3,9 +3,15 @@
 total = 0
 number = 0
 
+#Count to 1000
 while (number < 1000):
+  #Increment if it is a multiple of 5 or 3
   if (number % 5 == 0 or number % 3 == 0):
+    #Add to total
     total += number
+    
+  #Increment number
   number+=1
 
+#Print total
 print(total)
