8e568d93cb6d96df274166027f55b127ae915e34
André Barata <andre.quina.barata@gmail.com>
2015-09-30 14:55:59 +0000

    Using words array

--- Lab3/Palindrome.py
+++ Lab3/Palindrome.py
@@ -2,5 +2,10 @@
 
 import sys
 
-for word in sys.argv[1:]:
-  print (word[::-1] == word);
+#for word in sys.argv[1:]:
+#  print (word[::-1] == word)
+
+words = []
+
+for word in words:
+  print (word[::-1] == word)
