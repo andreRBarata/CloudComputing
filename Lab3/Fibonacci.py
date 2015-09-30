25aee6e98419344bdd1e55b03e591ffcc6275132
André Barata <andre.quina.barata@gmail.com>
2015-09-30 16:16:46 +0000

    Remove Old Code

--- Lab3/Fibonacci.py
+++ Lab3/Fibonacci.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-total = 0
-
 old = 1
 new = 1
 
@@ -9,10 +7,7 @@ count = 2
 
 while (len(str(new)) < 1000):
   next = old + new
-
-  if (next % 2 == 0):
-    total += next
-
+  
   count += 1
 
   old = new
