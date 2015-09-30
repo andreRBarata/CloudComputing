76ae7b41a14e9119353a4190f4272a307e41aa56
André Barata <andre.quina.barata@gmail.com>
2015-09-30 14:58:44 +0000

    Adding words to word array

--- Lab3/Palindrome.py
+++ Lab3/Palindrome.py
@@ -5,7 +5,10 @@ import sys
 #for word in sys.argv[1:]:
 #  print (word[::-1] == word)
 
-words = []
+words = [
+  "Oxo", "OXO", "123454321",
+  "ROTATOR", "12345  54321"
+]
 
 for word in words:
   print (word[::-1] == word)
