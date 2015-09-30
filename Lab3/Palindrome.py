8dafb6fc0d17211273c5efcfe34bcf4fa3fe7ebe
André Barata <andre.quina.barata@gmail.com>
2015-09-30 15:03:57 +0000

    Adding comments

--- Lab3/Palindrome.py
+++ Lab3/Palindrome.py
@@ -2,13 +2,16 @@
 
 import sys
 
+# Version using system arguments
 #for word in sys.argv[1:]:
 #  print (word[::-1] == word)
 
+# Words to test
 words = [
   "Oxo", "OXO", "123454321",
   "ROTATOR", "12345  54321"
 ]
 
+# Tests each word
 for word in words:
   print (word[::-1] == word)
